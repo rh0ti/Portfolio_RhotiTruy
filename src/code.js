c1c2adc41bab15bb437e66a84501169e8d381192
Rhottana Truy <rhottana.truy@gmail.com>
2022-11-30 16:40:47 +0100

    added changes

--- src/code.js
+++ src/code.js
@@ -1,4 +1,38 @@
+//-------------------------------------------------------------- Intro ---------------------------------------------------------------//
 
+const intro = document.querySelector('.intro');
+const logo = document.querySelector('.logo-header');
+const logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+  setTimeout(()=>{
+
+    logoSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+        span.classList.add('active');
+      }, (idx + 1) * 800)
+    });
+
+    setTimeout(()=>{
+
+      logoSpan.forEach((span, idx)=>{
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.add('fade');
+        }, (idx + 1) * 50)
+      })
+      
+      },3800);
+
+      setTimeout(()=>{
+        intro.style.top = '-100vh';
+      },4600)
+
+  })
+})
+
+console.log(logoSpan);
 //-------------------------------------------------------------- ScrollUp Button ---------------------------------------------------------------//
 $(document).ready(function (){
 
@@ -21,3 +55,45 @@ $(document).ready(function (){
       });
     
     });
+
+    
+    //-------------------------------------------------------------- Sidebar ---------------------------------------------------------------//
+
+$(document).ready(function(){
+
+    //By clicking the Burgermenu (SidebarBtn), the sidebar will show. When click on Bergermenu again it will close.(Toggle)
+        $('.sidebarBtn').click(function(){
+                  //$(console.log) - Debug-Logs
+            $('.sidebar').toggleClass('active');
+            $('.sidebarBtn').toggleClass('toggle');
+        })
+
+        $('.sidebar a').click(function(){
+          //$(console.log) - Debug-Logs
+          $('.sidebar').toggleClass('active');
+          $('.sidebarBtn').toggleClass('toggle');
+    })
+
+        
+    
+    })
+
+//-------------------------------------------------------------- Circle ---------------------------------------------------------------//
+
+// const text = document.querySelector('.text p');
+// text.innerHTML = text.innerText.split("").map(
+//   (char, i) =>
+//   `<span style="transform:rotate(${i * 7}deg)">${char}</span>`
+// ).join("")
+
+
+//-------------------------------------------------------------- Hover Effect ---------------------------------------------------------------//
+
+
+new hoverEffect({
+  parent: document.querySelector('#header-title .distortion'),
+  intensity: 0.2,
+  image1: '/images/bw1.png',
+  image2: '/images/bw2.png',
+  displacementImage: '/images/diss.png'
+});
